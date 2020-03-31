a1c9db181760bdf235af8f3143f6276a0c9d3ed4
Mariam Haider <mfh.haider@gmail.com>
2020-03-31 02:53:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,20 +7,21 @@ class Dog
 		@name = name
 		@@all << self
 	end
-
-	def self.clear_all
-		@@all.clear
-
-	end
-
-	def self.all
+	
+  def self.all
 		@@all
 	end
 	
+	def self.clear_all
+		@@all.clear
+  end
+
 	def self.print_all
 	  @@all.each do |dog|
 			puts dog.name
+			end
 	end
+	
 	
 end 
  
